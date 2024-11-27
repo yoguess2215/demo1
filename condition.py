226766cba639fa1d6cd41f3eb0eb59aca6145ea7
yoguess2215 <yogeshnandath@gmail.com>
2024-11-27 20:01:28 +0530

    added condition.py and added line to index

---
+++ condition.py
@@ -0,0 +1,11 @@
+
+def checkOddEven(x):
+    if(x % 2 == 0):
+      print("It is even")
+      return 
+    else:
+        print("It is odd")
+        # Complete the statement below
+        return
+
+x = checkOddEven(6)
